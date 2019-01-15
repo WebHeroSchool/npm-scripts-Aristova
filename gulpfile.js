311aaa99e7c9000362dc20b738d235dbf1fcaa1b
Irina Aristova <irinaristova7@gmail.com>
2019-01-15 11:19:34 +0300

    new

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,21 @@ const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const cssnano = require('gulp-cssnano');
 const sourcemaps = require('gulp-sourcemaps');
+const path ={
+        src:{
+                styles:'src/styles',
+                scripts:'src/build'
+        },
+        new:{
+                styles:'new-css',
+                scripts:'new-js'
+        },
+        buildNames:{
+                styles:'mainstyles.min.css',
+                scripts:'index.min.js'
+        }
+};
 
-gulp.task('date', () => console.log(new Date));
 
 gulp.task('build', () => {
         return gulp.src('new/hello.js')
@@ -28,25 +41,30 @@ gulp.task('build', () => {
 });
 
 gulp.task('new-js', () => {
-        return gulp.src(['build/*.js'])
+        return gulp.src([path.src.scripts])
                 .pipe(sourcemaps.init())
-                .pipe(concat('index.js'))
+                .pipe(concat(path.buildNames.scripts))
                 .pipe(babel({
                         presets: ['@babel/env']
                 }))
                 .pipe(uglify())
                 .pipe(sourcemaps.write())
-                .pipe(gulp.dest('new'))
+                .pipe(gulp.dest(path.new.scripts))
 });
 
 gulp.task('new-css', () => {
-        return gulp.src(['styles/*.css'])
+        return gulp.src([path.src.styles])
                 .pipe(sourcemaps.init())
-                .pipe(concat('mainstyles.css'))
+                .pipe(concat(path.buildNames.styles))
                 .pipe(cssnano())
                 .pipe(sourcemaps.write())
-                .pipe(gulp.dest('new'))
+                .pipe(gulp.dest(path.new.styles))
 
 });
 
 gulp.task('default', ['new-js', 'new-css']);
+
+gulp.task('watch',()=>{
+        gulp.watch(path.src.scripts,['new-js']);
+        gulp.watch(path.src.styles,['new-css']);
+});
