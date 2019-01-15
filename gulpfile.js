1afe0fa29134a70dd9eab743544d95a2a5ecc5e1
Irina Aristova <irinaristova7@gmail.com>
2019-01-15 13:03:09 +0300

    new

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,24 @@ const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const cssnano = require('gulp-cssnano');
 const sourcemaps = require('gulp-sourcemaps');
-const path ={
-        src:{
-                styles:'src/styles',
-                scripts:'src/build'
+const browserSync = require('browser-sync').create();
+
+const paths = {
+        src: {
+                styles: 'src/styles/*.css',
+                scripts: 'src/build/*.js'
         },
-        new:{
-                styles:'new-css',
-                scripts:'new-js'
+        new: {
+                styles: 'new',
+                scripts: 'new'
         },
-        buildNames:{
-                styles:'mainstyles.min.css',
-                scripts:'index.min.js'
+        buildNames: {
+                styles: 'mainstyles.min.css',
+                scripts: 'index.min.js'
         }
 };
+
+
 
 
 gulp.task('build', () => {
@@ -41,30 +45,42 @@ gulp.task('build', () => {
 });
 
 gulp.task('new-js', () => {
-        return gulp.src([path.src.scripts])
+        return gulp.src([paths.src.scripts])
                 .pipe(sourcemaps.init())
-                .pipe(concat(path.buildNames.scripts))
+                .pipe(concat(paths.buildNames.scripts))
                 .pipe(babel({
                         presets: ['@babel/env']
                 }))
                 .pipe(uglify())
                 .pipe(sourcemaps.write())
-                .pipe(gulp.dest(path.new.scripts))
+                .pipe(gulp.dest(paths.new.scripts))
 });
 
 gulp.task('new-css', () => {
-        return gulp.src([path.src.styles])
+        return gulp.src([paths.src.styles])
                 .pipe(sourcemaps.init())
-                .pipe(concat(path.buildNames.styles))
+                .pipe(concat(paths.buildNames.styles))
                 .pipe(cssnano())
                 .pipe(sourcemaps.write())
-                .pipe(gulp.dest(path.new.styles))
+                .pipe(gulp.dest(paths.new.styles))
 
 });
 
 gulp.task('default', ['new-js', 'new-css']);
 
-gulp.task('watch',()=>{
-        gulp.watch(path.src.scripts,['new-js']);
-        gulp.watch(path.src.styles,['new-css']);
+// gulp.task('watch', () => {
+//         gulp.watch(paths.src.scripts['new-js']);
+//         gulp.watch(paths.src.styles['new-css']);
+// });
+
+gulp.task('browser-sync', () => {
+        browserSync.init({
+                server: {
+                        baseDir: "./new"
+                }
+        });
+        gulp.watch(paths.src.scripts, ['new-js-watch']);
+        gulp.watch(paths.src.styles, ['new-css-watch']);
 });
+gulp.task('new-js-watch', ['new-js'], () => browserSync.reload());
+gulp.task('new-css-watch', ['new-css'], () => browserSync.reload());
